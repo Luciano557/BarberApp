d1dbe2e9b3fef24655de5cd62b0afabe9b086420
Luciano557 <garcialuciano557@gmail.com>
2026-07-22 14:12:54 -0300

    cambio de imagen

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -275,7 +275,7 @@ export function AppSidebar({ activeTab, onTabChange }: AppSidebarProps) {
         >
           <div className="relative flex items-center justify-center">
             <img
-              src="/IsotipoBlanco.PNG"
+              src="/MagotipoBlanco.PNG"
               alt="Vittro"
               title={railMode ? organization?.name || 'Barbería' : undefined}
               className="h-14 w-14 shrink-0 object-contain"
